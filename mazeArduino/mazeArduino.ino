92ca1476d4f1057fc4eed6627df413bc58344a03
Memocana <Memo@Memows-MacBook-Air.local>
2015-03-22 12:04:33 +0200

    √(zıkkım) = ık√(zm)

--- mazeArduino/mazeArduino.ino
+++ mazeArduino/mazeArduino.ino
@@ -7,7 +7,7 @@ void setup() {
   Serial.begin(9600);
   x_axis.attach(A0);
   z_axis.attach(A1);
-  x_axis.write(50);
+  x_axis.write(100);
   z_axis.write(0);
 }
 
@@ -15,10 +15,10 @@ void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.readStringUntil('\n');
     int xpos = (incomingByte.substring(incomingByte.indexOf(':')+1)).toInt();
-    incomingByte = Serial.readStringUntil('\n');
-    int zpos = (incomingByte.substring(incomingByte.indexOf(':')+1)).toInt();
+    Serial.println(xpos);
+    /*incomingByte = Serial.readStringUntil('\n');
+    int zpos = (incomingByte.substring(incomingByte.indexOf(':')+1)).toInt();*/
     x_axis.write(xpos);
-    z_axis.write(zpos);
+    //z_axis.write(zpos);
   }
-  delay(25);
 }
